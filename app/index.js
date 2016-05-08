cdd61245c8b07ad077a186085df48ea84683bf2c
Paolo D'Apice <paolo@hashtag.be>
2016-05-08 22:49:04 +0800

    Sample todo app, console only

--- app/index.js
+++ app/index.js
@@ -1,1 +1,27 @@
-// TODO: todo app here
+import todoApp from './reducers'
+
+let store = Redux.createStore(todoApp)
+
+/* Sample app here */
+
+import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from './actions'
+
+// Log initial state
+console.log(store.getState())
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(completeTodo(0))
+store.dispatch(completeTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+unsubscribe()
