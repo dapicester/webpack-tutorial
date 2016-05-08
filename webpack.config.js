c7abcf392de4605b8d2661b09b412badd690525e
Paolo D'Apice <paolo@hashtag.be>
2016-05-08 20:18:23 +0800

    Clean webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,4 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: [
@@ -20,6 +15,10 @@ module.exports = {
         ]
     },
     plugins: [
-        HtmlWebpackPluginConfig
+        new htmlWebpackPlugin({
+            template: __dirname + '/app/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
     ]
 };
