f8f59f3a1f3d7ade16443a62114abc1dc0ef593c
Paolo D'Apice <paolo@hashtag.be>
2016-05-08 19:48:34 +0800

    Friend list

--- app/index.js
+++ app/index.js
@@ -1,31 +1,76 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 
-const Hello = React.createClass({
+const FriendsContainer = React.createClass({
     getInitialState: function() {
         return {
-            username: 'paolo'
+            name: "Paolo",
+            friends: ["Pippo", "Pluto", "Paperino"]
         };
     },
 
-    handleChange: function(ev) {
+    addFriend: function(friend) {
         this.setState({
-            username: ev.target.value
-        })
+            friends: this.state.friends.concat([friend])
+        });
     },
 
     render: function() {
         return (
             <div>
-                Hello {this.state.username},<br/>
-                Change name:
-                <input type="text" value={this.state.username} onChange={this.handleChange} />
+                <h3>Name: {this.state.name}</h3>
+                <ShowList names={this.state.friends} />
+                <AddFriend addNew={this.addFriend} />
+            </div>
+        );
+    }
+});
+
+const AddFriend = React.createClass({
+    getInitialState: function() {
+        return {
+            newFriend: ''
+        };
+    },
+
+    updateNewFriend: function(ev) {
+        this.setState({
+            newFriend: ev.target.value
+        });
+    },
+
+    handleAddNew: function() {
+        this.props.addNew(this.state.newFriend); // this calls the addNew prop, which is a function
+        this.setState({
+            newFriend: ''
+        });
+    },
+
+    render: function() {
+        return (
+            <div>
+                <input type="text" value={this.state.newFriend} onChange={this.updateNewFriend} />
+                <button onClick={this.handleAddNew}>Add Friend</button>
+            </div>
+        );
+    }
+});
+
+const ShowList = React.createClass({
+    render: function() {
+        const listItems = this.props.names.map(friend => {
+            return <li>{friend}</li>;
+        });
+        return (
+            <div>
+                <h3>Friends:</h3>
+                <ul>{listItems}</ul>
             </div>
         );
     }
 });
 
 ReactDOM.render(
-    <Hello />,
+    <FriendsContainer />,
     document.getElementById('app')
 );
