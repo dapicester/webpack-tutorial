3a8c347539e48cf3bda1831f36c94ee0797e84b1
Paolo D'Apice <paolo@hashtag.be>
2016-05-08 19:22:29 +0800

    Add state and change handler

--- app/index.js
+++ app/index.js
@@ -2,9 +2,25 @@ const React = require("react");
 const ReactDOM = require("react-dom");
 
 const Hello = React.createClass({
+    getInitialState: function() {
+        return {
+            username: 'paolo'
+        };
+    },
+
+    handleChange: function(ev) {
+        this.setState({
+            username: ev.target.value
+        })
+    },
+
     render: function() {
         return (
-            <div>It works, with React!</div>
+            <div>
+                Hello {this.state.username},<br/>
+                Change name:
+                <input type="text" value={this.state.username} onChange={this.handleChange} />
+            </div>
         );
     }
 });
